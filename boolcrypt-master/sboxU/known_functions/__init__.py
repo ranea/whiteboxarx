9391669ed8d9935c89c509cd7ab7fc3f72d52f14
Adrián Ranea <adrian.ranea@esat.kuleuven.be>
2022-04-03 12:43:51 +0200

    moved files

--- boolcrypt-master/sboxU/known_functions/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Contains various functions from the literature on vectorial Boolean
-functions/S-boxes.
-
-"""
-
-from . import sixBitAPN
-from . import sevenBitAPN
-from . import eightBitAPN
-from . import tenBitAPN
